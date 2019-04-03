1837ca802ebffe8789b74d47d7dcd366ee3c0bd2
Sheena <sheenamwong@gmail.com>
2019-04-02 21:33:45 -0500

    Wrote in code for homework. Answer is CAT.

--- wheel_of_fortune.js
+++ wheel_of_fortune.js
@@ -1,45 +1,25 @@
-// wheel_of_fortune.js
-// CoderGirl WebDev (Winter 2019) — LaunchCode
+// Ask player if they would like to guess a letter or guess the solution
+console.log("Would you like to guess a letter or guess the solution?");
 
-// VARIABLES /////////////////////////////////////////////////////////
+// Variables
+var word = 'CAT';
+var isGuessingLetter = true;
+var letter = 'F';
 
-// In a normal application you would ask for user input.
-// In this exercise we will set the variables ourselves.
+// Guessing letter
+if (isGuessingLetter === true && letter === 'C') {
+  console.log("C--");
+} else if (isGuessingLetter === true && letter === 'A') {
+  console.log("-A-");
+} else if (isGuessingLetter === true && letter === 'T') {
+  console.log("--T");
+} else if (isGuessingLetter === true && letter != 'C' && letter != 'A' && letter != 'T') {
+  console.log("Sorry, no " + letter + "!")
+}
 
-// Set a secret word!
-
-var word = 'ANSWER'; // ALL CAPS
-
-// Ask player if they would like to guess a letter or guess the
-// solution.
-
-var isGuessingLetter = true; // set to false to guess solution
-
-// If they are guessing a letter, set the letter that they are
-// guessing.
-
-var letter = 'A'; // UPPERCASE
-
-// SCRIPT ////////////////////////////////////////////////////////////
-
-// If guessing a letter...
-
-	// If the word contains the letter...
-
-		// Print back the word with all instances of that letter revealed.
-
-	// If the word does not contain the letter...
-
-		// Print back "Sorry, no <letter>!" but with <letter> replaced by the player's guess.
-
-// If guessing the word...
-
-	// If they are correct...
-
-		// Print "Congratulations! You won Wheel Of Fortune!"
-
-	// If they are incorrect...
-
-		// Print back "Nope, keep trying!"
-
-// End of script!
+// Guessing solution
+if (isGuessingLetter === false && word === 'CAT') {
+    console.log("Congratulations! You won Wheel Of Fortune!");
+  } else if (isGuessingLetter === false && word != 'CAT') {
+    console.log("Nope, keep trying!");
+  }
